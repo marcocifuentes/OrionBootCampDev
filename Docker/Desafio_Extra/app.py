234e1a82c75347c55e6bebdb8aff1ba38c019e0b
Marco Cifuentes <marco.cifuentes@gmail..com>
2025-11-05 17:52:00 -0300

    feat: :sparkles: Adiciona Desafio Extra (Adminer + Healthcheck)

---
+++ Docker/Desafio_Extra/app.py
@@ -0,0 +1,50 @@
+import os
+import psycopg2
+from flask import Flask
+import time
+
+app = Flask(__name__)
+
+def get_db_connection():
+    # Pega as variáveis de ambiente que o docker-compose injetou
+    host = os.environ.get('DB_HOST')
+    dbname = os.environ.get('POSTGRES_DB')
+    user = os.environ.get('POSTGRES_USER')
+    password = os.environ.get('POSTGRES_PASSWORD')
+    port = os.environ.get('DB_PORT')
+    
+    retries = 10
+    while retries > 0:
+        try:
+            conn = psycopg2.connect(
+                host=host,
+                database=dbname,
+                user=user,
+                password=password,
+                port=port
+            )
+            return conn
+        except psycopg2.OperationalError:
+            retries -= 1
+            print(f"Erro de conexão. Tentando novamente... {retries} tentativas restantes.")
+            time.sleep(2) # Espera 2 segundos
+    return None # Retorna None se falhar
+
+@app.route('/')
+def hello():
+    conn = get_db_connection()
+    if conn:
+        conn.close()
+        return "Conexão com o banco de dados PostgreSQL bem-sucedida!"
+    else:
+        return "Falha ao conectar ao banco de dados.", 500
+
+# CORREÇÃO: Função adicionada com a indentação correta
+@app.route('/health')
+def health_check():
+    # Critério do Desafio
+    return {"status": "ok"}
+
+# Roda o servidor na porta 5000 (interna do container)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
